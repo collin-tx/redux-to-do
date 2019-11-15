2f183c02010671e9bc15940ed0500dd17a36d477
Collin Smith <cmsmith.tx@gmail.com>
2019-11-15 14:53:58 -0600

    splitting reducers

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -6,7 +6,16 @@ import {  ADD_TODO,
 
 const initialState = {
   visibilityFilter: VisibilityFilters.SHOW_ALL,
-  todos: []
+  todos: [
+    {
+      text: 'Consider using Redux',
+      completed: true
+    },
+    {
+      text: 'Keep all state in a single tree',
+      completed: false
+    }
+  ]
 }
 
 const todos(state = [], action) => {
